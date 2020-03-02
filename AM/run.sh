6433253daa459ae3cddfb88426a4a7308aab6e7a
Arun <arun.verma@valvoline.com>
2020-03-02 16:31:16 +0530

    change

--- AM/run.sh
+++ AM/run.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Run OpenAM - optionally configuring the image.
 # Please see the comments inline to understand how this
-cd /opt/forgerock/tomcat
 echo "Starting tomcat"
-
-
 
 /opt/forgerock/tomcat/bin/catalina.sh run

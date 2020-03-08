cbecee0e83d1008ce07144b7e93a2b412f9e5148
Arun <arun.verma@valvoline.com>
2020-03-08 21:27:18 +0530

    latest update

--- DS/Bootstrap/usr/setup.sh
+++ DS/Bootstrap/usr/setup.sh
@@ -9,7 +9,7 @@ echo "Setting up default OpenDJ instance"
   #--baseDN $BASE_DN -h localhost --rootUserPassword "$PASSWORD" \
  # --acceptLicense --no-prompt --sampleData 2
 
-/opt/forgerock/opendj_usr/opendj/setup directory-server --rootUserDn cn=DirectoryManager --rootUserPassword Inside@123   --monitorUserDn uid=Monitor --monitorUserPassword Inside@123 --hostname nausawsodj601.corp.valvoline.com    --doNotStart    --adminConnectorPort $adminConnectorPort    --ldapPort $ldapPort   --enableStartTls  --ldapsPort $ldapsPort  --httpsPort $httpsPort  --addBaseEntry  --baseDn dc=valvoline,dc=com --acceptLicense
+/opt/forgerock/opendj_usr/opendj/setup directory-server --rootUserDn cn=DirectoryManager --rootUserPassword Inside@123   --monitorUserDn uid=Monitor --monitorUserPassword Inside@123 --hostname $host    --doNotStart    --adminConnectorPort $adminConnectorPort    --ldapPort $ldapPort   --enableStartTls  --ldapsPort $ldapsPort  --httpsPort $httpsPort  --addBaseEntry  --baseDn dc=valvoline,dc=com --acceptLicense
 
 
 /opt/forgerock/opendj_usr/opendj/bin/backup  --hostname nausawsodj603.corp.valvoline.com  --port 4444  --bindDN "cn=DirectoryManager"  --bindPassword "Inside@123" --backUpAll  --backupDirectory /opt/forgerock/OpenDJ_USR/opendj/bak --backUpAll --recurringTask "00 02 * * *" --completionNotify arun.verma@valvoline.com --errorNotify arun.verma@valvoline.com  --trustAll
